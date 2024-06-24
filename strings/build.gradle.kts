55df193377e4ffe855b9d12b4b007f5cbae32123
Mehdi <mm.nikfarjam@gmail.com>
2024-06-24 10:59:14 +0000

    Add test coverage and sonar cloud scan

--- strings/build.gradle.kts
+++ strings/build.gradle.kts
@@ -11,11 +11,13 @@ plugins {
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+    jacoco
 }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+sonar {
+    properties {
+        property("sonar.coverage.jacoco.xmlReportPaths","build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
 
 dependencies {
@@ -26,22 +28,4 @@ dependencies {
     testImplementation(libs.junit.jupiter.engine)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
-}
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
 }
