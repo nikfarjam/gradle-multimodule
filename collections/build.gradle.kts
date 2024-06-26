af37e429e2a6ecb9d0af01d416931ca9d7751740
Mehdi <mm.nikfarjam@gmail.com>
2024-06-26 02:54:26 +0000

    Publish coverage

--- collections/build.gradle.kts
+++ collections/build.gradle.kts
@@ -24,3 +24,9 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+sonar {
+    properties {
+        property ("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
